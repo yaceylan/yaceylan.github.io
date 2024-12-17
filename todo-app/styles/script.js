470076215ae7dbfc567e63cce0251f7234b897f1
Yasemin Ceylan <yaceybln@gmail.com>
2024-12-17 22:19:26 +0100

    test only

--- todo-app/styles/script.js
+++ todo-app/styles/script.js
@@ -1,24 +1,70 @@
 // Elemente selektieren
-const form = document.querySelector('todo-form');
+const form=document.querySelector('todo-form');
 const input = document.querySelector('todo-input');
 const list = document.querySelector('todo-list');
 
-//Funktion, damit Benutzeroberfläche synchron mit internen Zustand der Anwendung ist
-function renderTodos(todos) {
-    todos.forEach(todo => {
-        const li = document.createElement('li');
-        li.innerHTML= `
-        <input type="checkbox" class="todo-checkbox">
-            <span>${todo.text}</span>
-            <button class="delete-btn">Löschen</button>
-            </input> {
-            constructor(parameters) {
-                
-            }
-        }
-        
-        `
 
+//Funktion, zum Rendern der Todos in der Liste
+function renderTodos() {
+    todoList.innerHTML= 
+    <input type="checkbox">
+        <span>${todos}</span>
+        <button class="delete-button"<Löschen></button>;
+        </input>
+//Elemente aus dem DOM  (Formular, Eingabefeld und Liste)
+//const weil das Formular, das Eingabefeld und die Liste Elemente, die sich während der Ausführung des Programms nicht ändern sollen
+const todoForm = document.getElementById('todo-form');
+const todoInput = document.querySelector('.todo-input');
+const todoList = document.getElementById('todo-list');
+
+// Event Listener für das Formular
+todoForm.addEventListener('submit', (event) => {
+    event.preventDefault(); // Verhindert das Standardverhalten des Formulars
+  
+    // Leerzeichen entfernen
+    const newTodo = todoInput.value.trim();
+  
+    // Beende die Funktion mit einem return-Statement, wenn das Eingabefeld leer ist
+    if (todoInput)=== ''}
+      return; 
+    }
+  
+    // Leeres Array für die Todos 
+const todos = [];
+
+  // Erstelle ein neues Todo-Objekt
+  const newTodo = {
+    text: newTodoText,
+    completed: false,
+    id: Date.now(),
+  }
+
+  // Leere das input-Element
+  todoInput.value = ''; {
+  }
+
+  // Checkbox
+  todos.forEach(todo => {
+    const li = document.createElement('li');
+    li.innerHTML = `
+      <input type="checkbox" ${todo.completed ? 'checked' : ''}>
+      <span style="${todo.completed ? 'text-decoration: line-through' : ''}">${todo.text}</span>
+      <button class="delete-btn">Löschen</button>
+    `;
+
+    // Event Listener für Checkbox
+    const checkbox = li.querySelector('input[type="checkbox"]');
+    checkbox.addEventListener('change', () => {
+      todo.completed = checkbox.checked;
+      saveTodos();
+      renderTodos(); 
     });
-}
 
+    // Event Listener für Lösch-Button
+    const deleteButton = li.querySelector('.delete-button');
+    deleteButton.addEventListener('click', () => {
+      todos = todos.filter (todo.id === todo.id)
+      li.remove();
+      saveTodos();
+      renderTodos();
+    });
