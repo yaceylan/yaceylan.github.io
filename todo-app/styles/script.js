1cf7605a4d536f2615df605fac59cdee017ae107
Yasemin Ceylan <yaceybln@gmail.com>
2024-12-18 22:58:52 +0100

    added save function

--- todo-app/styles/script.js
+++ todo-app/styles/script.js
@@ -5,14 +5,27 @@ const button = document.getElementById("submit");
 
 let toDos = []
 
-button.addEventListener ("click", ()=>{
+button.addEventListener("click", () => {
   const submit = document.createElement("li")
   submit.textContent = "Hinzufügen"
 
-  form.addEventListener ("todo-form", ()=>{
+  form.addEventListener("todo-form", () => {
     event.preventDefault();
   })
- 
 
-let input = newTask.value.trim();
+
+  let input = newTask.value.trim();
 })
+
+
+function saveTodos(todos) {
+  localStorage.setItem("todos")
+}
+
+if (todos < 1) {
+}
+else {
+  console.log("Keine Aufgaben")
+}
+console.log(JSON.stringify({ toDos }));
+
